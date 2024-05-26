e4397d14bd3b8bfb5f1cf1dc9354b91a60000a68
TheJp202 <jeanpierrellc.jpllc@gmail.com>
2024-05-26 17:45:04 -0500

    Actualizacion app Proyectos

--- Proyecto/views.py
+++ Proyecto/views.py
@@ -5,16 +5,39 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,parser_classes
 from rest_framework.parsers import MultiPartParser, FormParser
-
-
+from django.http import JsonResponse
+from ProyectoTarea.models import ProyectoTarea
+from ProyectoTarea.serializers import ProyectoTareaSerializer
 
 @api_view(['GET', 'POST'])
 @parser_classes([MultiPartParser, FormParser])
 def proyectoLC(request):
     if request.method == 'GET':
-        proyecto = Proyecto.objects.all()
-        serializer = ProyectoSerializer(proyecto, many=True)
-        return Response(serializer.data)
+        proyectos = Proyecto.objects.all()
+        response_data = []
+        for proyecto in proyectos:
+            proyecto_data = {
+        "id": proyecto.id,
+        "Nombre": proyecto.Nombre,
+        "Imagen": proyecto.Imagen.url,
+        "Descripcion": proyecto.Descripcion,
+        "FechaInicio": proyecto.FechaInicio,
+        "FechaFinal": proyecto.FechaFinal,
+        "PctTareas": 0
+            }
+            tareas = ProyectoTarea.objects.filter(Proyecto=proyecto.id)
+            TotalTareas = len(tareas)
+            if TotalTareas != 0:
+                RealizadasTareas = 0
+                for tarea in tareas:
+                    if(tarea.Estado == "Realizada"):
+                        RealizadasTareas += 1
+                PorcentajeTareas = round((RealizadasTareas/TotalTareas)*100,2)
+            else:
+                PorcentajeTareas = 0
+            proyecto_data["PctTareas"] = PorcentajeTareas
+            response_data.append(proyecto_data)
+        return JsonResponse(response_data, safe=False)
     elif request.method == 'POST':
         serializer = ProyectoSerializer(data=request.data)
         if serializer.is_valid():
@@ -22,6 +45,8 @@ def proyectoLC(request):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
     
+
+
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 @parser_classes([MultiPartParser, FormParser])
